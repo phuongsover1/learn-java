d0480f5a88ae7e609f717395bce60ce09b97c1bb
Phuong Nguyen <phuongsover2@gmail.com>
2024-11-06 22:31:45 +0700

    init project

---
+++ modern-api-book/demo/converting_oas_to_spring_code/src/main/java/com/example/converting_oas_to_spring_code/ConvertingOasToSpringCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.converting_oas_to_spring_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertingOasToSpringCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertingOasToSpringCodeApplication.class, args);
+	}
+
+}
---
+++ modern-api-book/demo/converting_oas_to_spring_code/src/test/java/com/example/converting_oas_to_spring_code/ConvertingOasToSpringCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.converting_oas_to_spring_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertingOasToSpringCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

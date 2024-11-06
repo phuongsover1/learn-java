4cdf5a7d336558745de76487630cf0047489a106
Phuong Nguyen <phuongsover2@gmail.com>
2024-11-06 23:22:42 +0700

    init project for converting OAS to Spring code

--- modern-api-book/converting_oas_to_spring_code/src/main/java/com/pakt/modern/api/ConvertingOasToSpringCodeApplication.java
+++ modern-api-book/converting_oas_to_spring_code/src/main/java/com/pakt/modern/api/ConvertingOasToSpringCodeApplication.java
@@ -1,4 +1,4 @@
-package com.example.converting_oas_to_spring_code;
+package com.pakt.modern.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

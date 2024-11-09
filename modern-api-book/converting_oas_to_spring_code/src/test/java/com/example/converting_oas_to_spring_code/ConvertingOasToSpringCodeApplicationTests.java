9bfd8a181b3e457c32223aa57e1964412615e054
Phuong Nguyen <phuongsover2@gmail.com>
2024-11-09 11:57:00 +0700

    bla

--- modern-api-book/converting_oas_to_spring_code/src/test/java/com/example/converting_oas_to_spring_code/ConvertingOasToSpringCodeApplicationTests.java
+++ modern-api-book/converting_oas_to_spring_code/src/test/java/com/example/converting_oas_to_spring_code/ConvertingOasToSpringCodeApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.converting_oas_to_spring_code;
 
+import com.pakt.modern.api.ConvertingOasToSpringCodeApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ConvertingOasToSpringCodeApplication.class)
 class ConvertingOasToSpringCodeApplicationTests {
 
 	@Test

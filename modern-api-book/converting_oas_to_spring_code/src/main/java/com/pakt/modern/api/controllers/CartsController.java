329f5ed7c1b8ff399973618c4f0ae8851b8072c5
Phuong Nguyen <phuongsover2@gmail.com>
2024-11-09 11:57:11 +0700

    Implementing the OAS code interfaces

---
+++ modern-api-book/converting_oas_to_spring_code/src/main/java/com/pakt/modern/api/controllers/CartsController.java
@@ -0,0 +1,34 @@
+package com.pakt.modern.api.controllers;
+
+import com.packt.modern.api.CartApi;
+import com.packt.modern.api.model.Cart;
+import com.packt.modern.api.model.Item;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CartsController implements CartApi {
+
+  private static final Logger log = LoggerFactory.getLogger(CartsController.class);
+
+
+  @Override
+  public ResponseEntity<List<Item>> addCartItemsByCustomerId(String customerId, @Valid Item item) throws Exception {
+    log.info("Request for customer ID: {}\nItem: {}", customerId, item);
+    return ResponseEntity.ok(Collections.EMPTY_LIST);
+  }
+
+  @Override
+  public ResponseEntity<Cart> getCartByCustomerId(String customerId) throws Exception {
+    throw new RuntimeException("Manual Exception thrown");
+  }
+
+}

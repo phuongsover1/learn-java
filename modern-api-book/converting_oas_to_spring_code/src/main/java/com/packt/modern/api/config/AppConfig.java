e5f54a73e738c1f057d83f1d69a6b46f6078e88e
Phuong Nguyen <phuongsover2@gmail.com>
2024-12-08 10:12:22 +0700

    Adding ETags technique to save bandwidth

---
+++ modern-api-book/converting_oas_to_spring_code/src/main/java/com/packt/modern/api/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.packt.modern.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class AppConfig {
+  @Bean
+  public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+    return new ShallowEtagHeaderFilter();
+  }
+}

88e58053cd7d890af583d135eaf354829de05ef6
Phuong Nguyen <phuongsover2@gmail.com>
2024-12-26 11:35:30 +0700

    Enhancing the UserService class

--- modern-api-book/converting_oas_to_spring_code/src/main/java/com/packt/modern/api/service/UserService.java
+++ modern-api-book/converting_oas_to_spring_code/src/main/java/com/packt/modern/api/service/UserService.java
@@ -3,6 +3,9 @@ package com.packt.modern.api.service;
 import com.packt.modern.api.entity.AddressEntity;
 import com.packt.modern.api.entity.CardEntity;
 import com.packt.modern.api.entity.UserEntity;
+import com.packt.modern.api.model.RefreshToken;
+import com.packt.modern.api.model.SignedInUser;
+import com.packt.modern.api.model.User;
 
 import java.util.Optional;
 
@@ -12,5 +15,9 @@ public interface UserService {
   Iterable<UserEntity> getAllCustomers();
   Optional<CardEntity> getCardByCustomerId(String id);
   Optional<UserEntity> getCustomerById(String id);
-
+  UserEntity findUserByUsername(String username);
+  Optional<SignedInUser> createUser(User user);
+  SignedInUser getSignedInUser(UserEntity userEntity);
+  Optional<SignedInUser> getAccessToken(RefreshToken refToken);
+  void removeRefreshToken(RefreshToken refToken);
 }

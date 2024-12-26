fe889f273fa38118ab27705709134edc527fa440
Phuong Nguyen <phuongsover2@gmail.com>
2024-12-26 11:35:39 +0700

    Implementing findUserByUsername()

--- modern-api-book/converting_oas_to_spring_code/src/main/java/com/packt/modern/api/repository/UserRepository.java
+++ modern-api-book/converting_oas_to_spring_code/src/main/java/com/packt/modern/api/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.packt.modern.api.repository;
 
 import com.packt.modern.api.entity.UserEntity;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends CrudRepository<UserEntity, UUID> {
+  Optional<UserEntity> findByUsername(@NotNull(message = "User name is required.") String username);
 }
--- modern-api-book/converting_oas_to_spring_code/src/main/java/com/packt/modern/api/service/UserServiceImpl.java
+++ modern-api-book/converting_oas_to_spring_code/src/main/java/com/packt/modern/api/service/UserServiceImpl.java
@@ -4,10 +4,11 @@ import com.packt.modern.api.entity.AddressEntity;
 import com.packt.modern.api.entity.CardEntity;
 import com.packt.modern.api.entity.UserEntity;
 import com.packt.modern.api.repository.UserRepository;
-import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 import java.util.UUID;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -34,11 +35,23 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public Optional<CardEntity> getCardByCustomerId(String id) {
-    return Optional.of(userRepository.findById(UUID.fromString(id)).map(UserEntity::getCard).get().get(0));
+    return Optional.of(
+        userRepository.findById(UUID.fromString(id)).map(UserEntity::getCard).get().get(0));
   }
 
   @Override
   public Optional<UserEntity> getCustomerById(String id) {
     return userRepository.findById(UUID.fromString(id));
+  }
+
+  @Override
+  public UserEntity findUserByUsername(String username) {
+    if (Strings.isBlank(username)) {
+      throw new UsernameNotFoundException("Invalid user");
+    }
+    final String uname = username.trim();
+    Optional<UserEntity> oUserEntity = userRepository.findByUsername(uname);
+    return oUserEntity.orElseThrow(
+        () -> new UsernameNotFoundException(String.format("Given user %s not found.", uname)));
   }
 }

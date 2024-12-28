50c3f865a16087acd4f3ca8b2ee074ebdabad1af
Phuong Nguyen <phuongsover2@gmail.com>
2024-12-28 16:21:38 +0700

    install react-router-dom

--- modern-api-book/ecomm-ui/src/App.js
+++ modern-api-book/ecomm-ui/src/App.js
@@ -9,7 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Phuong Nguyen Duy Le</p>
         <a
           className="App-link"
           href="https://reactjs.org"

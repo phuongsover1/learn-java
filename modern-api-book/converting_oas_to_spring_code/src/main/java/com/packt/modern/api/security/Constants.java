d4c0829923b4fc09ca91bb3c13e7fbcf222e9365
Phuong Nguyen <phuongsover2@gmail.com>
2024-12-28 21:41:45 +0700

    Fix error that require old access token when I want to get new accessToken using refreshToken

--- modern-api-book/converting_oas_to_spring_code/src/main/java/com/packt/modern/api/security/Constants.java
+++ modern-api-book/converting_oas_to_spring_code/src/main/java/com/packt/modern/api/security/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
   public static final String H2_URL_PREFIX = "/h2-console/**";
   public static final String SIGNUP_URL = "/api/v1/customers";
   public static final String TOKEN_URL = "/api/v1/auth/token";
-  public static final String REFRESH_URL = "/api/v1/auh/token/refresh";
+  public static final String REFRESH_URL = "/api/v1/auth/token/refresh";
   public static final String PRODUCTS_URL = "/api/v1/products/**";
   public static final String AUTHORIZATION = "Authorization";
   public static final String TOKEN_PREFIX = "Bearer ";

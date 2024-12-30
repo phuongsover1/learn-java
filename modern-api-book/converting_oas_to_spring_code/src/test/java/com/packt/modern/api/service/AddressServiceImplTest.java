70d33fa6a86b4a0ccbc09b9aef21d5a2bd2e00c6
Phuong Nguyen <phuongsover2@gmail.com>
2024-12-30 21:08:28 +0700

    Added getAddressById() unit test

--- modern-api-book/converting_oas_to_spring_code/src/test/java/com/packt/modern/api/service/AddressServiceImplTest.java
+++ modern-api-book/converting_oas_to_spring_code/src/test/java/com/packt/modern/api/service/AddressServiceImplTest.java
@@ -1,22 +1,5 @@
 package com.packt.modern.api.service;
 
-import com.packt.modern.api.entity.AddressEntity;
-import com.packt.modern.api.exceptions.ResourceNotFoundException;
-import com.packt.modern.api.model.AddAddressReq;
-import com.packt.modern.api.repository.AddressRepository;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,38 +9,63 @@ import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import com.packt.modern.api.entity.AddressEntity;
+import com.packt.modern.api.exceptions.ResourceNotFoundException;
+import com.packt.modern.api.model.AddAddressReq;
+import com.packt.modern.api.repository.AddressRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
 @ExtendWith(MockitoExtension.class)
 class AddressServiceImplTest {
-  private final static String id = "a1b9b31d-e73c-4112-af7c-b68530f38222";
-  private final static String nonExistId = "a1b9b31d-e73c-4112-af7c-b68530f38220";
+  private static final String id = "a1b9b31d-e73c-4112-af7c-b68530f38222";
+  private static final String nonExistId = "a1b9b31d-e73c-4112-af7c-b68530f38220";
   private static AddressEntity entity;
   private static AddAddressReq addressReq;
-  @Mock
-  private AddressRepository repository;
-  @InjectMocks
-  private AddressServiceImpl service;
+  @Mock private AddressRepository repository;
+  @InjectMocks private AddressServiceImpl service;
 
   @BeforeAll
   public static void setup() {
-    entity = new AddressEntity()
+    entity =
+        new AddressEntity()
             .setId(UUID.fromString(id))
-            .setNumber("111").setResidency("Residency").setStreet("Street")
-            .setCity("City").setCountry("Country").setPincode("12345");
-    addressReq = new AddAddressReq().id(entity.getId().toString()).number(entity.getNumber())
-            .residency(entity.getResidency()).street(entity.getStreet()).city(entity.getCity())
-            .country(entity.getCountry()).pincode(entity.getPincode());
+            .setNumber("111")
+            .setResidency("Residency")
+            .setStreet("Street")
+            .setCity("City")
+            .setCountry("Country")
+            .setPincode("12345");
+    addressReq =
+        new AddAddressReq()
+            .id(entity.getId().toString())
+            .number(entity.getNumber())
+            .residency(entity.getResidency())
+            .street(entity.getStreet())
+            .city(entity.getCity())
+            .country(entity.getCountry())
+            .pincode(entity.getPincode());
   }
 
   @Test
-  @DisplayName("returns an AddressEntity when private method toEntity() is called with AddAddressReq model")
+  @DisplayName(
+      "returns an AddressEntity when private method toEntity() is called with AddAddressReq model")
   public void convertModelToEntity() {
     // given
     AddressServiceImpl srvc = new AddressServiceImpl(repository);
     // when
-     AddressEntity e =  ReflectionTestUtils.invokeMethod(srvc, "toEntity", addressReq);
-     // then
-    then(e).as("Check address entity is returned & not null")
-            .isNotNull();
+    AddressEntity e = ReflectionTestUtils.invokeMethod(srvc, "toEntity", addressReq);
+    // then
+    then(e).as("Check address entity is returned & not null").isNotNull();
     then(e.getNumber()).as("Check house/flat number is set").isEqualTo(entity.getNumber());
     then(e.getResidency()).as("Check residency is set").isEqualTo(entity.getResidency());
     then(e.getStreet()).as("Check street is set").isEqualTo(entity.getStreet());
@@ -84,21 +92,25 @@ class AddressServiceImplTest {
   @Test
   @DisplayName("delete address by given non-existing id, should throw ResourceNotFoundException")
   public void deleteAddressesByNonExistId() {
-   given(repository.findById(UUID.fromString(nonExistId))).willReturn(Optional.empty())
-           .willThrow(new ResourceNotFoundException(String.format("No Address found with id %s", nonExistId)));
+    given(repository.findById(UUID.fromString(nonExistId)))
+        .willReturn(Optional.empty())
+        .willThrow(
+            new ResourceNotFoundException(
+                String.format("No Address found with id %s", nonExistId)));
 
-   // when
+    // when
     try {
       service.deleteAddressById(nonExistId);
     } catch (Exception e) {
       // then
       assertThat(e).isInstanceOf(ResourceNotFoundException.class);
-      assertThat(e.getMessage()).isEqualTo(String.format("No Address found with id %s", nonExistId));
+      assertThat(e.getMessage())
+          .isEqualTo(String.format("No Address found with id %s", nonExistId));
     }
 
     // then
     verify(repository, times(1)).findById(UUID.fromString(nonExistId));
-    verify(repository,times(0)).deleteById(UUID.fromString(nonExistId));
+    verify(repository, times(0)).deleteById(UUID.fromString(nonExistId));
   }
 
   @Test
@@ -122,13 +134,42 @@ class AddressServiceImplTest {
     given(repository.save(any())).willReturn(entity);
 
     // when
-   Optional<AddressEntity> result = service.createAddress(addressReq);
+    Optional<AddressEntity> result = service.createAddress(addressReq);
 
-   // then
+    // then
     assertThat(result).isNotNull();
     assertThat(result).isNotEmpty();
     assertThat(result.get()).isEqualTo(entity);
+  }
 
+  @Test
+  @DisplayName("return address by given exist address id")
+  public void getAddressByExistId() {
+    // given
+    given(repository.findById(UUID.fromString(id))).willReturn(Optional.of(entity));
+
+    // when
+    Optional<AddressEntity> result = service.getAddressById(id);
+
+    // then
+    assertThat(result).isNotNull();
+    assertThat(result).isNotEmpty();
+    assertThat(result.get()).isEqualTo(entity);
+    verify(repository,times(1)).findById(UUID.fromString(id));
+  }
+
+  @Test
+  @DisplayName("return empty address by given non-existing id")
+  public void getAddressByNonExistId() {
+    // given
+    given(repository.findById(UUID.fromString(nonExistId))).willReturn(Optional.empty());
+
+    // when
+    Optional<AddressEntity> result = service.getAddressById(nonExistId);
+
+    // then
+    assertThat(result).isNotNull();
+    assertThat(result).isEmpty();
+    verify(repository, times(1)).findById(UUID.fromString(nonExistId));
   }
 }
-

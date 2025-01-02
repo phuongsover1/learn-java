2894dfada75dfb9c073c80c3d65173619e7e0d04
Phuong Nguyen <phuongsover2@gmail.com>
2025-01-02 20:22:52 +0700

    init project

---
+++ modern-api-book/gRPC/client/src/main/java/com/packt/modern/api/Chapter11GrpcApplication.java
@@ -0,0 +1,13 @@
+package com.packt.modern.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter11GrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter11GrpcApplication.class, args);
+	}
+
+}

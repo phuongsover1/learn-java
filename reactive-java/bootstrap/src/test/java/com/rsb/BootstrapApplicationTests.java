006306436baa4ab2def264cef3f562525c4ace1e
nero <phuongsover2@gmail.com>
2025-01-18 19:47:42 +0700

    init project

---
+++ reactive-java/bootstrap/src/test/java/com/rsb/BootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

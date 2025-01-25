90d6993a1aca9f6c8db4276a1bdeaab8c85364ac
nero <phuongsover2@gmail.com>
2025-01-25 10:39:38 +0700

    The beanGrpcServerRunnerTest() method tests the loading of the GrpcServer class and GrpcServerRunner and the test should pass if the profile is set correctly.

---
+++ modern-api-book/gRPC/server/src/main/java/com/packt/modern/api/server/GrpcServer.java
@@ -0,0 +1,62 @@
+package com.packt.modern.api.server;
+
+import com.packt.modern.api.server.interceptor.ExceptionInterceptor;
+import com.packt.modern.api.server.service.ChargeService;
+import com.packt.modern.api.server.service.SourceService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GrpcServer {
+
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
+  @Value("${grpc.port}")
+  private int port;
+  private Server server;
+  private final ChargeService chargeService;
+  private final SourceService sourceService;
+  private final ExceptionInterceptor exceptionInterceptor;
+
+  public GrpcServer(ChargeService chargeService, SourceService sourceService, ExceptionInterceptor exceptionInterceptor) {
+    this.chargeService = chargeService;
+    this.sourceService = sourceService;
+    this.exceptionInterceptor = exceptionInterceptor;
+  }
+
+  public void start() throws IOException {
+    logger.info("Starting server on port {}", port);
+    server = ServerBuilder.forPort(port)
+            .addService(sourceService)
+            .addService(chargeService)
+            .intercept(exceptionInterceptor)
+            .build().start();
+    logger.info("gRPC server started and listening on port: {}", port);
+    logger.info("Following server are available: ");
+    server.getServices().stream()
+            .forEach(s -> logger.info("Service Name: {}", s.getServiceDescriptor().getName()));
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      logger.info("Shutting down gRPC server");
+      GrpcServer.this.stop();
+      logger.info("gRPC server shut down successfully!!!");
+    }));
+  }
+
+  public void stop() {
+    if (server != null) {
+      server.shutdown();
+    }
+  }
+
+  public void block() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+}
--- modern-api-book/gRPC/server/src/test/java/com/packt/modern/api/Chapter11GrpcApplicationTests.java
+++ modern-api-book/gRPC/server/src/test/java/com/packt/modern/api/Chapter11GrpcApplicationTests.java
@@ -1,13 +1,36 @@
 package com.packt.modern.api;
 
+import com.packt.modern.api.server.GrpcServer;
+import com.packt.modern.api.server.GrpcServerRunner;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ActiveProfiles("test")
 @SpringBootTest
-class Chapter11GrpcApplicationTests {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ServerAppTests {
+
+	@Autowired
+	private ApplicationContext context;
 
 	@Test
-	void contextLoads() {
+	@Order(1)
+	void beanGrpcServerRunnerTest() {
+		assertNotNull(context.getBean(GrpcServer.class));
+		assertThrows(NoSuchBeanDefinitionException.class, () -> context.getBean(GrpcServerRunner.class),
+						"GrpcServerRunner should not be loaded during test");
+
 	}
+
 
 }

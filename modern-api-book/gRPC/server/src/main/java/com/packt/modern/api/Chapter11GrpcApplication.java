9adc0de70181d992ceddc0c75b29bb09df17ea0a
Phuong Nguyen <phuongsover2@gmail.com>
2025-01-31 18:11:26 +0700

    reformat files

--- modern-api-book/gRPC/server/src/main/java/com/packt/modern/api/Chapter11GrpcApplication.java
+++ modern-api-book/gRPC/server/src/main/java/com/packt/modern/api/Chapter11GrpcApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Chapter11GrpcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Chapter11GrpcApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(Chapter11GrpcApplication.class, args);
+  }
 }

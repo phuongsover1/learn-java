0bff896faa3b6c05b5b75f2aa9fdfbdfc675eeda
nero <phuongsover2@gmail.com>
2025-03-01 09:19:49 +0700

    init project

---
+++ java-persistence-with-spring-data-and-hibernate/Chapter2-helloworld/src/main/java/com/learn/hibernate/Chapter2HelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.learn.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter2HelloworldApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Chapter2HelloworldApplication.class, args);
+  }
+
+}
---
+++ java-persistence-with-spring-data-and-hibernate/Chapter2-helloworld/src/test/java/com/learn/hibernate/Chapter2HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter2HelloworldApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

dbf37f0583ca9bc396378ec86d7a5582b2add090
Nero <phuongsover2@gmail.com>
2025-05-21 23:40:42 +0700

    create user entity

---
+++ java-persistence-with-spring-data-and-hibernate/springdatajpa-chapter4/src/main/java/com/manning/javapersistence/springdatajpa/model/User.java
@@ -0,0 +1,58 @@
+package com.manning.javapersistence.springdatajpa.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private LocalDate registrationDate;
+
+    public User() {}
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public User(Long id, String username, LocalDate registrationDate) {
+        this.id = id;
+        this.username = username;
+        this.registrationDate = registrationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", registrationDate=" + registrationDate +
+                '}';
+    }
+}

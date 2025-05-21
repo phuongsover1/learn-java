2a20a95aaeaf14de3246be56b1439c8ea2c1422e
Nero <phuongsover2@gmail.com>
2025-05-22 00:21:48 +0700

    The UserRepository interface with new methods for filter detailed information

--- java-persistence-with-spring-data-and-hibernate/springdatajpa-chapter4/src/main/java/com/manning/javapersistence/springdatajpa/repositories/UserRepository.java
+++ java-persistence-with-spring-data-and-hibernate/springdatajpa-chapter4/src/main/java/com/manning/javapersistence/springdatajpa/repositories/UserRepository.java
@@ -1,7 +1,26 @@
 package com.manning.javapersistence.springdatajpa.repositories;
 
 import com.manning.javapersistence.springdatajpa.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    List<User> findAllByOrderByUsernameDesc();
+    List<User> findByRegistrationDateBetween(LocalDate start, LocalDate end);
+    List<User> findByUsernameAndEmail(String username, String email);
+    List<User> findByUsernameOrEmail(String username, String email);
+    List<User> findByLevelOrderByUsernameDesc(int level);
+    List<User> findByUsernameContaining(String text);
+    List<User> findByUsernameLike(String text);
+    List<User> findByUsernameStartingWith(String text);
+    List<User> findByUsernameEndingWith(String text);
+    List<User> findByActive(boolean active);
+    List<User> findByRegistrationDateIn(Collection<LocalDate> dates);
+    List<User> findByRegistrationDateNotIn(Collection<LocalDate> dates);
+
 }

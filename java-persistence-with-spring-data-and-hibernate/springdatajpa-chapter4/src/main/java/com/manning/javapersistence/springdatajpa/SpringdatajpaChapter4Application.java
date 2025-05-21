8bb38f406ddb7108bc2049d678aec4e9ab6e3256
Nero <phuongsover2@gmail.com>
2025-05-21 23:19:58 +0700

    init project

---
+++ java-persistence-with-spring-data-and-hibernate/springdatajpa-chapter4/src/main/java/com/manning/javapersistence/springdatajpa/SpringdatajpaChapter4Application.java
@@ -0,0 +1,13 @@
+package com.manning.javapersistence.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaChapter4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaChapter4Application.class, args);
+	}
+
+}
---
+++ java-persistence-with-spring-data-and-hibernate/springdatajpa-chapter4/src/test/java/com/manning/javapersistence/springdatajpa/SpringdatajpaChapter4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manning.javapersistence.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpaChapter4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

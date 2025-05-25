16c327319301f9e402babaefa5d9a55b3f2e8b59
Nero <phuongsover2@gmail.com>
2025-05-25 16:02:01 +0700

    Overriding access strategy for the name property

--- java-persistence-with-spring-data-and-hibernate/Ch06/mapping-value-types/src/main/java/com/manning/javapersistence/ch06/model/Item.java
+++ java-persistence-with-spring-data-and-hibernate/Ch06/mapping-value-types/src/main/java/com/manning/javapersistence/ch06/model/Item.java
@@ -1,13 +1,23 @@
 package com.manning.javapersistence.ch06.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Item {
   @Id
   @GeneratedValue(generator = "ID_GENERATOR")
   private Long id;
+
+  @Access(AccessType.PROPERTY)
+  @Column(name = "ITEM_NAME")
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = !name.startsWith("AUCTION: ") ? "AUCTION: " + name : name;
+  }
 
 }

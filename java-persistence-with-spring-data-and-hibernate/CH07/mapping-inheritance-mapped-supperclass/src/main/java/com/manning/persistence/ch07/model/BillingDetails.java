e200734c78b84e8db94f44fadb43ecbd02b83736
Phuong Nguyen <phuongsover2@gmail.com>
2025-12-17 22:23:57 +0700

    Mapping BillingDetails (abstract superclass) with implicit polymorphism

---
+++ java-persistence-with-spring-data-and-hibernate/CH07/mapping-inheritance-mapped-supperclass/src/main/java/com/manning/persistence/ch07/model/BillingDetails.java
@@ -0,0 +1,32 @@
+package com.manning.persistence.ch07.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+
+@MappedSuperclass
+public class BillingDetails {
+    @Id
+    @GeneratedValue(generator = "ID_GENERATOR")
+    private Long id;
+
+    @NotNull
+    private String owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}

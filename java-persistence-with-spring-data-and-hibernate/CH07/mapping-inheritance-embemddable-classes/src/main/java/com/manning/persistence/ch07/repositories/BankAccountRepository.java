838f53ae9c935373ab001177fa6d6e9bd486139b
Phuong Nguyen <phuongsover2@gmail.com>
2025-12-18 18:58:28 +0700

    Init Chapter07-Inheritance of embeddable classes

--- java-persistence-with-spring-data-and-hibernate/CH07/mapping-inheritance-embemddable-classes/src/main/java/com/manning/persistence/ch07/repositories/BankAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.manning.persistence.ch07.repositories;
-
-import com.manning.persistence.ch07.model.BankAccount;
-
-import java.util.List;
-
-public interface BankAccountRepository extends BillingDetailsRepository<BankAccount, Long> {
-    List<BankAccount> findBySwift(String swift);
-}
--- java-persistence-with-spring-data-and-hibernate/CH07/mapping-inheritance-embemddable-classes/src/main/java/com/manning/persistence/ch07/repositories/CreditCardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.manning.persistence.ch07.repositories;
-
-import com.manning.persistence.ch07.model.CreditCard;
-
-import java.util.List;
-
-public interface CreditCardRepository extends BillingDetailsRepository<CreditCard, Long> {
-    List<CreditCard> findByExpYear(String expYear);
-}

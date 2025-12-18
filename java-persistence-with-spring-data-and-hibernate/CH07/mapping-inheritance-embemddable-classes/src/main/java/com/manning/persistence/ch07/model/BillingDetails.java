d8d03b419b045ee0c2dd80885624bddb3c762513
Phuong Nguyen <phuongsover2@gmail.com>
2025-12-18 18:53:28 +0700

    Init Chapter07-Inheritance of embeddable classes

---
+++ java-persistence-with-spring-data-and-hibernate/CH07/mapping-inheritance-embemddable-classes/src/main/java/com/manning/persistence/ch07/model/BillingDetails.java
@@ -0,0 +1,38 @@
+package com.manning.persistence.ch07.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class BillingDetails {
+    @Id
+    @GeneratedValue(generator = "ID_GENERATOR")
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false)
+    private String owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    protected BillingDetails() {}
+
+    protected BillingDetails(String owner) {
+        this.owner = owner;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}

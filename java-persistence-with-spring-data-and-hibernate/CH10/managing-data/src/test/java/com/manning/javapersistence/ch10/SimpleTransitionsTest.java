9107b41452b93b0425024ce2dbe8e7a3613ade61
Phuong Nguyen <phuongsover2@gmail.com>
2025-12-28 16:37:21 +0700

    10.2.4 Getting a reference

--- java-persistence-with-spring-data-and-hibernate/CH10/managing-data/src/test/java/com/manning/javapersistence/ch10/SimpleTransitionsTest.java
+++ java-persistence-with-spring-data-and-hibernate/CH10/managing-data/src/test/java/com/manning/javapersistence/ch10/SimpleTransitionsTest.java
@@ -105,4 +105,89 @@ public class SimpleTransitionsTest {
         em.getTransaction().commit();
         em.close();
     }
+
+    @Test
+    void retrievePersistenceReference() {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Item someItem = new Item();
+        someItem.setName("Some Item");
+        em.persist(someItem);
+        em.getTransaction().commit();
+        em.close();
+        Long ITEM_ID = someItem.getId();
+        Long NON_EXISTENT_ID = ITEM_ID + 999;
+
+        {
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+
+              /*
+               If the persistence context already contains an <code>Item</code> with the given identifier, that
+               <code>Item</code> instance is returned by <code>getReference()</code> without hitting the database.
+               Furthermore, if <em>no</em> persistent instance with that identifier is currently managed, a hollow
+               placeholder will be produced by Hibernate, a proxy. This means <code>getReference()</code> will not
+               access the database, and it doesn't return <code>null</code>, unlike <code>find()</code>.
+             */
+            Item item = em.getReference(Item.class, ITEM_ID);
+
+             /*
+               JPA offers <code>PersistenceUnitUtil</code> helper methods such as <code>isLoaded()</code> to
+               detect if you are working with an uninitialized proxy.
+            */
+            PersistenceUnitUtil persistenceUnitUtil = emf.getPersistenceUnitUtil();
+
+            assertFalse(persistenceUnitUtil.isLoaded(item));
+
+            /*
+               As soon as you call any method such as <code>Item#getName()</code> on the proxy, a
+               <code>SELECT</code> is executed to fully initialize the placeholder. The exception to this rule is
+               a method that is a mapped database identifier getter method, such as <code>getId()</code>. A proxy
+               might look like the real thing but it is only a placeholder carrying the identifier value of the
+               entity instance it represents. If the database record doesn't exist anymore when the proxy is
+               initialized, an <code>EntityNotFoundException</code> will be thrown.
+             */
+            // assertEquals("Some Item", item.getName());
+
+            // We can initialize the proxy explicitly using Hibernate API like below
+            /*
+               Hibernate has a convenient static <code>initialize()</code> method, loading the proxy's data.
+             */
+//         Hibernate.initialize(item);
+
+//            assertEquals("Some Item", item.getName()); // DB hit occurs here
+
+            em.getTransaction().commit();
+            em.close();
+
+
+             /*
+               After the persistence context is closed, <code>item</code> is in detached state. If you do
+               not initialize the proxy while the persistence context is still open, you get a
+               <code>LazyInitializationException</code> if you access the proxy. You can't load
+               data on-demand once the persistence context is closed. The solution is simple: Load the
+               data before you close the persistence context.
+             */
+            assertThrows(LazyInitializationException.class, () -> {
+                item.getName(); // Outside of transaction and EntityManager: LazyInitializationException
+            });
+        }
+
+        {
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+
+            EntityManager finalEm = em;
+            assertThrows(javax.persistence.EntityNotFoundException.class, () -> {
+                Item item = finalEm.getReference(Item.class, NON_EXISTENT_ID);
+                item.getName(); // DB hit occurs here
+            });
+
+            em.getTransaction().commit();
+            em.close();
+        }
+
+    }
+
+
 }

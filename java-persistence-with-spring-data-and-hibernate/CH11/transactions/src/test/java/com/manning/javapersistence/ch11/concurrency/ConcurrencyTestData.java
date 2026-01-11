64c6af273cd786467275e23fc2ef6d397e27bc39
Phuong Nguyen <phuongsover2@gmail.com>
2026-01-11 12:05:03 +0700

    Manual version checking

---
+++ java-persistence-with-spring-data-and-hibernate/CH11/transactions/src/test/java/com/manning/javapersistence/ch11/concurrency/ConcurrencyTestData.java
@@ -0,0 +1,6 @@
+package com.manning.javapersistence.ch11.concurrency;
+
+public class ConcurrencyTestData {
+  TestData categories;
+  TestData items;
+}
--- java-persistence-with-spring-data-and-hibernate/CH11/transactions/src/test/java/com/manning/javapersistence/ch11/concurrency/Versioning.java
+++ java-persistence-with-spring-data-and-hibernate/CH11/transactions/src/test/java/com/manning/javapersistence/ch11/concurrency/Versioning.java
@@ -2,11 +2,10 @@ package com.manning.javapersistence.ch11.concurrency;
 
 import org.junit.jupiter.api.Test;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.OptimisticLockException;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
@@ -15,6 +14,31 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Versioning {
   EntityManagerFactory emf = Persistence.createEntityManagerFactory("ch11");
+
+  ConcurrencyTestData storeCategoriesAndItems() {
+    EntityManager em = emf.createEntityManager();
+    em.getTransaction().begin();
+
+    ConcurrencyTestData testData = new ConcurrencyTestData();
+    testData.categories = new TestData(new Long[3]);
+    testData.items = new TestData(new Long[5]);
+    for (int i = 1; i <= testData.categories.identifiers.length; i++) {
+      Category category = new Category();
+      category.setName("Category: " + i);
+      em.persist(category);
+      testData.categories.identifiers[i - 1] = category.getId();
+      for (int j = 1; j <= testData.categories.identifiers.length; j++) {
+        Item item = new Item("Item " + j);
+        item.setCategory(category);
+        item.setBuyNowPrice(new BigDecimal(10 + j));
+        em.persist(item);
+        testData.items.identifiers[(i - 1) + (j - 1)] = item.getId();
+      }
+    }
+    em.getTransaction().commit();
+    em.close();
+    return testData;
+  }
 
   @Test
   void firstCommitWins() throws ExecutionException, InterruptedException {
@@ -77,5 +101,85 @@ public class Versioning {
     */
     assertThrows(OptimisticLockException.class, () -> em1.flush());
     // update ITEM set NAME = ?, VERSION = 1 where ID = ? and VERSION = 0
+  }
+
+  @Test
+  void manualVersionChecking() throws ExecutionException, InterruptedException {
+    final ConcurrencyTestData testData = storeCategoriesAndItems();
+    Long[] CATEGORIES = testData.categories.identifiers;
+
+    EntityManager em = emf.createEntityManager();
+    em.getTransaction().begin();
+
+    BigDecimal totalPrice = BigDecimal.ZERO;
+    for (Long categoryId : CATEGORIES) {
+      /*
+        For each <code>Category</code>, query all <code>Item</code> instances with
+        an <code>OPTIMISTIC</code> lock mode. Hibernate now knows it has to
+        check each <code>Item</code> at flush time.
+      */
+      List<Item> items =
+          em.createQuery("select i from Item i where i.category.id = :catId", Item.class)
+              .setLockMode(LockModeType.OPTIMISTIC)
+              .setParameter("catId", categoryId)
+              .getResultList();
+      for (Item item : items) {
+        totalPrice = totalPrice.add(item.getBuyNowPrice());
+        // Will trigger an optimistic lock check at transaction commit time
+//        if (item.getId().equals(testData.items.getFirstId())) {
+//          item.setBuyNowPrice(BigDecimal.valueOf(1));
+        }
+      }
+
+
+      // Now a concurrent transaction will move an item to another category
+      if (categoryId.equals(testData.categories.getFirstId())) {
+        Executors.newSingleThreadExecutor()
+            .submit(
+                () -> {
+                  try {
+                    EntityManager em2 = emf.createEntityManager();
+                    em2.getTransaction().begin();
+
+                    // Moving the first item from the first category into the last category
+                    List<Item> items1 =
+                        em2.createQuery(
+                                "select i from Item i where i.category.id = :catId", Item.class)
+                            .setParameter("catId", testData.categories.getFirstId())
+                            .getResultList();
+
+                    Category lastCategory =
+                        em2.getReference(Category.class, testData.categories.getLastId());
+
+                    items1.iterator().next().setCategory(lastCategory);
+
+                    em2.getTransaction().commit();
+                    em2.close();
+                  } catch (Exception ex) {
+                    // this should
+                    throw new RuntimeException("Concurrent operation failure: " + ex, ex);
+                  }
+                  return null;
+                })
+            .get();
+      }
+    }
+
+    em.getTransaction().commit();
+    em.close();
+    assertEquals("108.00", totalPrice.toString());
+
+    em = emf.createEntityManager();
+    em.getTransaction().begin();
+
+    List<Item> allItemsFirstCategory =
+        em.createQuery("select i from Item i where i.category.id = :catId", Item.class)
+            .setParameter("catId", testData.categories.getFirstId())
+            .getResultList();
+
+    em.getTransaction().commit();
+    em.close();
+
+    assertEquals(2, allItemsFirstCategory.size());
   }
 }
